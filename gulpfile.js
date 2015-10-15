c1aa5eee439c91a60a901cdb06b23a203ce2209f
gszabo <gabor.szabo@emarsys.com>
2015-10-15 23:37:53 +0200

    test(application): use co-mocha and introduce gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let gulp = require('gulp');
+let jscs = require('gulp-jscs');
+let jsmin = require('gulp-jsmin');
+let rename = require('gulp-rename');
+
+gulp.task('default', function() {
+  console.log('hello');
+});
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('co-mocha');
+
 var expect = require('chai').expect;
 var adminUsernameLister = require('../src');
 
