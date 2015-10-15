112b8e5156cf38c207200f05d8654d6192442009
gszabo <gabor.szabo@emarsys.com>
2015-10-16 00:13:55 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,16 @@ let rename = require('gulp-rename');
 gulp.task('default', function() {
   console.log('hello');
 });
+
+gulp.task('code-style', function() {
+  return gulp.src(['src/**/*.js', 'test/**/*.js'])
+    .pipe(jscs())
+    .pipe(jscs.reporter());
+});
+
+gulp.task('minify', ['code-style'], function() {
+  return gulp.src(['src/**/*.js', 'test/**/*.js'])
+    .pipe(jsmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist'));
+});
